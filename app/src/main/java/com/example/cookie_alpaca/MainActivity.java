8ce5a49c12ddff1a9a3625da416f9c7feba95563
2tle <iam@2tle.io>
2021-04-01 20:50:32 +0900

    mainactivity src update

--- app/src/main/java/com/example/cookie_alpaca/MainActivity.java
+++ app/src/main/java/com/example/cookie_alpaca/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.cookie_alpaca;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    public int[] eightG = new int[16];
+    public int[] fourG = new int[8];
+    public int[] twoG = new int[4];
+    public int[] finalMatch = new int[2];
+    public int winner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Button leftBtn = findViewById(R.id.leftBtn);
+        //Button rightBtn = findViewById(R.id.rightBtn);
+
+        /* leftBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        }); */
+
+
+
     }
 }
